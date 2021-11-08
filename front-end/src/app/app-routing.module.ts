98bad804d3c363e2259b070ffa62d494b3c6bff9
Younes Adnane <younes.adnane4@gmail.com>
2021-11-08 09:32:12 +0100

    Personnaliser le scrollbar, Ajouter le header des sections 'top-nav-bar', Changer le font

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DiscoverComponent } from './components/pages/discover/discover.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "discover", component: DiscoverComponent},
+  {path: "", redirectTo: "discover", pathMatch: "full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeftNavBarComponent } from './components/partials/left-nav-bar/left-nav-bar.component';
+import { DiscoverComponent } from './components/pages/discover/discover.component';
+import { TopNavBarComponent } from './components/partials/top-nav-bar/top-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftNavBarComponent
+    LeftNavBarComponent,
+    DiscoverComponent,
+    TopNavBarComponent
   ],
   imports: [
     BrowserModule,
