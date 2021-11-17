0984064c180b7e7e839c1b6a66f0bdec12844886
Younes Adnane <younes.adnane4@gmail.com>
2021-11-17 09:37:41 +0100

    Configurer le passage des donnes des formations en tant qu'un tableau des formation au component d'apercu

--- front-end/src/app/services/binding/cv-info/cv-info.service.ts
+++ front-end/src/app/services/binding/cv-info/cv-info.service.ts
@@ -9,11 +9,21 @@ export class CvInfoService
 {
   public cv_info_subject = new BehaviorSubject<CV>(new CV());
   public cv_info = this.cv_info_subject.asObservable();
+  private cv: CV = new CV();
 
-  constructor() { }
+  constructor()
+  {
+    this.cv_info.subscribe(cv => this.cv = cv);
+  }
 
   public change_value(input: string, value: string)
   {
       this.cv_info_subject.next(Object.assign(this.cv_info_subject.value, {[input]: value}));
+  }
+
+  public change_value_trainings_diplomas(index: number, input: string, value: string)
+  {
+      this.cv.trainings_diplomas[index] = Object.assign(this.cv.trainings_diplomas[index], {[input]: value});
+      this.cv_info_subject.next(this.cv);
   }
 }
